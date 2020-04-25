14765fd16ce9f938a769f96e99994fc32df13397
William Furtunato <williamfurtunato@hotmail.com>
2020-04-25 15:20:06 -0300

    relationship between models appointments & users

--- src/database/migrations/1587831812456-CreateAppointments.ts
+++ src/database/migrations/1587831812456-CreateAppointments.ts
@@ -9,7 +9,7 @@ export default class CreateAppointments1587831812456
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
